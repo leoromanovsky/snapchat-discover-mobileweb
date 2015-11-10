384f34ca7da141193ae0c3c78aec98f89b9b896e
Leo Romanovsky <leoromanovsky@gmail.com>
2015-11-09 18:02:46 -0800

    very tall?

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ $(function () {
     windowHeight = $(document).height();
     windowWidth = $(document).width();
     $('#mask').css('width', windowWidth * numColumns);
-    $('#mask').css('height', windowHeight * numRows);
+    $('#mask').css('height', windowHeight * numRows + 10000);
     $('li').css('width', windowWidth);
     $('li').css('height', windowHeight);
     changeScreen(0);
--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ $(function() {
     windowWidth = $(document).width();
 
     $('#mask').css('width', windowWidth * numColumns);
-    $('#mask').css('height', windowHeight * numRows);
+    $('#mask').css('height', windowHeight * numRows + 10000);
     $('li').css('width', windowWidth);
     $('li').css('height', windowHeight);
 
