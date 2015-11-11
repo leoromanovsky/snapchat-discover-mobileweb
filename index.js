54629b3095575743fdec95c7e96c6ec11d82fd71
Leo Romanovsky <leoromanovsky@gmail.com>
2015-11-10 21:16:59 -0800

    lots of refactor

--- index.js
+++ index.js
@@ -5,18 +5,40 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-function instagramUrl(id) {
-    return "https://www.instagram.com/p/" + id + "/media?size=l";
+var mapboxAccessToken = "pk.eyJ1Ijoic3RyYXZhbHIiLCJhIjoiQjV5dllkNCJ9.Jso5bao3AOEvB1NjbkPJXg";
+function instagramUrl(id, requestedWidth) {
+    var size = 'l';
+    if (requestedWidth <= 320) {
+        size = 'm';
+    }
+    return "https://www.instagram.com/p/" + id + "/media?size=" + size;
+}
+function mapboxStaticMap(arg) {
+    return "https://api.mapbox.com/v4/mapbox.streets/" + arg + "?access_token=" + mapboxAccessToken;
+}
+function mapboxMapSize(height, width) {
+    return height + "x" + width;
 }
 var FullPageManager = (function () {
-    function FullPageManager() {
+    //pages: Array<Section>;
+    function FullPageManager(docHeight, docWidth) {
         this.firstPage = new FirstPage();
         this.lastPage = new LastPage();
-        this.pages = [new Section("<div class=\"section active\">\n      <div class=\"slide\">\n        <img src=\"" + instagramUrl('4QqH_uxRLf') + "\" width=\"375\" />\n      </div>\n      <div class=\"slide\">\n        <img src=\"https://api.mapbox.com/v4/mapbox.streets/path-5+f44+f44(acreF%60m%60jV%60A%60Dq%60@sh@eEeHH_DwJl@%7D~AzvAcJzMeGfP~G%60eA%7DCbH%60Cd%5E_CtEeG%60DEnEdDjItIrCjCjFuAdDOpMsFrKfRnoC%60CbHuC%60h@hFkC%7COt@~ApAMxEbCvAnBQbCkGhGjHrFKJuBiGqMDmEjFcFzAiHhIgFnD%7DNpIk@~D%60De@oKbDw@WqHxCm@oAgSlDk@%7BCqf@lIkA)/auto/375x627.png?access_token=pk.eyJ1Ijoic3RyYXZhbHIiLCJhIjoiQjV5dllkNCJ9.Jso5bao3AOEvB1NjbkPJXg\" width=\"375\" height=\"627\" />\n      </div>\n      <div class=\"slide\"><img src=\"" + instagramUrl('34rI74xRFj') + "\" width=\"375\" /></div>\n    </div>"),
-            new Section("\n        <div class=\"section\">\n          <div class=\"slide\">\n            <img src=\"https://api.mapbox.com/v4/mapbox.streets/path-5+f44+f44(kumwFrjvbMaf%40kuD%7BnCkS)/auto/375x627.png?access_token=pk.eyJ1Ijoic3RyYXZhbHIiLCJhIjoiQjV5dllkNCJ9.Jso5bao3AOEvB1NjbkPJXg\" width=\"375\" height=\"627\" />\n          </div>\n          <div class=\"slide\">\n            <img src=\"" + instagramUrl('4IZpIZRRHi') + "\" width=\"375\" />\n          </div>\n          <div class=\"slide\">\n            <img src=\"" + instagramUrl('371ql4RRC1') + "\" width=\"375\" />\n          </div>\n        </div>")];
+        this.docHeight = docHeight;
+        this.docWidth = docWidth;
     }
     FullPageManager.prototype.render = function () {
-        return "\n            " + this.firstPage.render() + "\n            " + this.pages.map(function (p) { return p.render(); }).join('') + "\n            " + this.lastPage.render() + "\n            ";
+        return "\n            " + this.firstPage.render() + "\n            " + this.pages().map(function (p) { return p.render(); }).join('') + "\n            " + this.lastPage.render() + "\n            ";
+    };
+    FullPageManager.prototype.documentWidth = function () {
+        return this.docWidth;
+    };
+    FullPageManager.prototype.documentHeight = function () {
+        return this.docHeight;
+    };
+    FullPageManager.prototype.pages = function () {
+        return [new Section("<div class=\"section active\">\n      <div class=\"slide\">\n        <img src=\"" + instagramUrl('4QqH_uxRLf', this.documentWidth()) + "\" width=\"" + this.documentWidth() + "\" />\n      </div>\n      <div class=\"slide\">\n        <img src=\"" + mapboxStaticMap("path-5+f44+f44(acreF%60m%60jV%60A%60Dq%60@sh@eEeHH_DwJl@%7D~AzvAcJzMeGfP~G%60eA%7DCbH%60Cd%5E_CtEeG%60DEnEdDjItIrCjCjFuAdDOpMsFrKfRnoC%60CbHuC%60h@hFkC%7COt@~ApAMxEbCvAnBQbCkGhGjHrFKJuBiGqMDmEjFcFzAiHhIgFnD%7DNpIk@~D%60De@oKbDw@WqHxCm@oAgSlDk@%7BCqf@lIkA)/auto/" + mapboxMapSize(this.documentWidth(), this.documentHeight()) + ".png") + "\" width=\"" + this.documentWidth() + "\" height=\"627\" />\n      </div>\n      <div class=\"slide\"><img src=\"" + instagramUrl('34rI74xRFj', this.documentWidth()) + "\" width=\"" + this.documentWidth() + "\" /></div>\n    </div>"),
+            new Section("\n        <div class=\"section\">\n          <div class=\"slide\">\n            <img src=\"" + mapboxStaticMap("path-5+f44+f44(kumwFrjvbMaf%40kuD%7BnCkS)/auto/" + mapboxMapSize(this.documentWidth(), this.documentHeight()) + ".png") + "\" width=" + this.documentWidth() + "\" height=\"627\" />\n          </div>\n          <div class=\"slide\">\n            <img src=\"" + instagramUrl('4IZpIZRRHi', this.documentWidth()) + "\" width=\"" + this.documentWidth() + "\" />\n          </div>\n          <div class=\"slide\">\n            <img src=\"" + instagramUrl('371ql4RRC1', this.documentWidth()) + "\" width=\"" + this.documentWidth() + "\" />\n          </div>\n        </div>")];
     };
     FullPageManager.prototype.initPlugin = function () {
         $('#fullpage').fullpage({
@@ -56,8 +78,10 @@ var LastPage = (function (_super) {
     };
     return LastPage;
 })(Section);
-var pageManager = new FullPageManager();
 $(function () {
+    var docHeight = $(document).height();
+    var docWidth = $(document).width();
+    var pageManager = new FullPageManager(docHeight, docWidth);
     $('#fullpage').html(pageManager.render());
     pageManager.initPlugin();
     setTimeout(function () {
--- index.ts
+++ index.ts
@@ -1,42 +1,77 @@
 /// <reference path="typings/jquery.d.ts"/>
 /// <reference path="typings/fullPage.d.ts"/>
 
-function instagramUrl(id): string {
-    return `https://www.instagram.com/p/${id}/media?size=l`
+var mapboxAccessToken: string = "pk.eyJ1Ijoic3RyYXZhbHIiLCJhIjoiQjV5dllkNCJ9.Jso5bao3AOEvB1NjbkPJXg";
+
+function instagramUrl(id, requestedWidth: number): string {
+    var size = 'l';
+    if (requestedWidth <= 320) {
+        size = 'm'
+    }
+
+    return `https://www.instagram.com/p/${id}/media?size=${size}`
+}
+
+function mapboxStaticMap(arg: string): string {
+    return `https://api.mapbox.com/v4/mapbox.streets/${arg}?access_token=${mapboxAccessToken}`
+}
+
+function mapboxMapSize(height: number, width: number): string {
+    return `${height}x${width}`
 }
 
 class FullPageManager {
+    private docHeight: number;
+    private docWidth: number;
+
     firstPage = new FirstPage();
     lastPage = new LastPage();
-    pages: Array<Section> = [new Section(
-        `<div class="section active">
-      <div class="slide">
-        <img src="${instagramUrl('4QqH_uxRLf')}" width="375" />
-      </div>
-      <div class="slide">
-        <img src="https://api.mapbox.com/v4/mapbox.streets/path-5+f44+f44(acreF%60m%60jV%60A%60Dq%60@sh@eEeHH_DwJl@%7D~AzvAcJzMeGfP~G%60eA%7DCbH%60Cd%5E_CtEeG%60DEnEdDjItIrCjCjFuAdDOpMsFrKfRnoC%60CbHuC%60h@hFkC%7COt@~ApAMxEbCvAnBQbCkGhGjHrFKJuBiGqMDmEjFcFzAiHhIgFnD%7DNpIk@~D%60De@oKbDw@WqHxCm@oAgSlDk@%7BCqf@lIkA)/auto/375x627.png?access_token=pk.eyJ1Ijoic3RyYXZhbHIiLCJhIjoiQjV5dllkNCJ9.Jso5bao3AOEvB1NjbkPJXg" width="375" height="627" />
-      </div>
-      <div class="slide"><img src="${instagramUrl('34rI74xRFj')}" width="375" /></div>
-    </div>`),
-        new Section(`
-        <div class="section">
-          <div class="slide">
-            <img src="https://api.mapbox.com/v4/mapbox.streets/path-5+f44+f44(kumwFrjvbMaf%40kuD%7BnCkS)/auto/375x627.png?access_token=pk.eyJ1Ijoic3RyYXZhbHIiLCJhIjoiQjV5dllkNCJ9.Jso5bao3AOEvB1NjbkPJXg" width="375" height="627" />
-          </div>
-          <div class="slide">
-            <img src="${instagramUrl('4IZpIZRRHi')}" width="375" />
-          </div>
-          <div class="slide">
-            <img src="${instagramUrl('371ql4RRC1')}" width="375" />
-          </div>
-        </div>`)];
+    //pages: Array<Section>;
+
+    constructor(docHeight: number, docWidth: number) {
+        this.docHeight = docHeight;
+        this.docWidth = docWidth;
+    }
 
     render(): string {
         return `
             ${this.firstPage.render()}
-            ${this.pages.map(function(p) {return p.render()} ).join('')}
+            ${this.pages().map(function(p) {return p.render()} ).join('')}
             ${this.lastPage.render()}
             `
+    }
+
+    documentWidth(): number {
+        return this.docWidth
+    }
+
+    documentHeight(): number {
+        return this.docHeight
+    }
+
+    pages(): Array<Section> {
+        return [new Section(
+            `<div class="section active">
+      <div class="slide">
+        <img src="${instagramUrl('4QqH_uxRLf', this.documentWidth())}" width="${this.documentWidth()}" />
+      </div>
+      <div class="slide">
+        <img src="${mapboxStaticMap(`path-5+f44+f44(acreF%60m%60jV%60A%60Dq%60@sh@eEeHH_DwJl@%7D~AzvAcJzMeGfP~G%60eA%7DCbH%60Cd%5E_CtEeG%60DEnEdDjItIrCjCjFuAdDOpMsFrKfRnoC%60CbHuC%60h@hFkC%7COt@~ApAMxEbCvAnBQbCkGhGjHrFKJuBiGqMDmEjFcFzAiHhIgFnD%7DNpIk@~D%60De@oKbDw@WqHxCm@oAgSlDk@%7BCqf@lIkA)/auto/${mapboxMapSize(this.documentWidth(), this.documentHeight())}.png`)}" width="${this.documentWidth()}" height="627" />
+      </div>
+      <div class="slide"><img src="${instagramUrl('34rI74xRFj', this.documentWidth())}" width="${this.documentWidth()}" /></div>
+    </div>`),
+            new Section(`
+        <div class="section">
+          <div class="slide">
+            <img src="${mapboxStaticMap(`path-5+f44+f44(kumwFrjvbMaf%40kuD%7BnCkS)/auto/${mapboxMapSize(this.documentWidth(), this.documentHeight())}.png`)}" width=${this.documentWidth()}" height="627" />
+          </div>
+          <div class="slide">
+            <img src="${instagramUrl('4IZpIZRRHi', this.documentWidth())}" width="${this.documentWidth()}" />
+          </div>
+          <div class="slide">
+            <img src="${instagramUrl('371ql4RRC1', this.documentWidth())}" width="${this.documentWidth()}" />
+          </div>
+        </div>`)]
     }
 
     initPlugin(): any {
@@ -92,9 +127,11 @@ class LastPage extends Section {
     }
 }
 
-var pageManager = new FullPageManager();
-
 $(function() {
+    var docHeight = $(document).height();
+    var docWidth = $(document).width();
+
+    var pageManager = new FullPageManager(docHeight, docWidth);
     $('#fullpage').html(pageManager.render());
     pageManager.initPlugin();
 
